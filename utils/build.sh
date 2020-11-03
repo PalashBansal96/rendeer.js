70a2c846f8007acdda9ff99523f47601dace25cc
Palash Bansal <palashbansal96@gmail.com>
2020-11-03 13:38:50 +0530

    Build update all, gitignore

--- utils/build.sh
+++ utils/build.sh
@@ -1,5 +1,6 @@
 cd "$(dirname "$0")"
 python builder.py deploy_files.txt -o ../build/rendeer.min.js -o2 ../build/rendeer.js
+python builder.py deploy_files_base.txt -o ../build/rendeer.base.min.js -o2 ../build/rendeer.base.js
 cat ../external/gl-matrix-min.js ../external/litegl.min.js ../build/rendeer.min.js > ../build/rendeer.full.min.js
 chmod a+rw ../build/* 
 #cp ../build/rendeer.js ../boilerplate/js/extra/rendeer.js
